00343de869cb014edd556c9455df119621f56cc5
Heedong <59307414+ruthetum@users.noreply.github.com>
2024-11-02 20:44:21 +0900

    chore(희동): playground 환경 셋업

---
+++ playground/ruthetum/app/case1/src/main/kotlin/team/sipe/spurt/app/case1/Case1Application.kt
@@ -0,0 +1,11 @@
+package team.sipe.spurt.app.case1
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Case1Application
+
+fun main(args: Array<String>) {
+    runApplication<Case1Application>(*args)
+}
---
+++ playground/ruthetum/settings.gradle.kts
@@ -0,0 +1,27 @@
+rootProject.name = "spurt"
+
+include(
+    "practice",
+    "practice:benchmark",
+    "app",
+    "app:case1",
+    "support",
+)
+
+pluginManagement {
+    val kotlinVersion: String by settings
+    val springBootVersion: String by settings
+    val springDependencyManagementVersion: String by settings
+
+    resolutionStrategy {
+        eachPlugin {
+            when (requested.id.id) {
+                "org.jetbrains.kotlin.jvm" -> useVersion(kotlinVersion)
+                "org.jetbrains.kotlin.plugin.spring" -> useVersion(kotlinVersion)
+                "org.jetbrains.kotlin.plugin.jpa" -> useVersion(kotlinVersion)
+                "org.springframework.boot" -> useVersion(springBootVersion)
+                "io.spring.dependency-management" -> useVersion(springDependencyManagementVersion)
+            }
+        }
+    }
+}
