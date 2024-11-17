5d43554861c2dcdb248dd478cd3a9166c3ee4901
woozi <kwj1270@naver.com>
2024-11-18 00:07:05 +0900

    feat(우재): 3주차 프로젝트 기본환경 세팅

---
+++ playground/kwj1270/commerce/refund/src/main/java/team/sipe/commerce/refund/RefundApplication.java
@@ -0,0 +1,13 @@
+package team.sipe.commerce.refund;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RefundApplication.class, args);
+    }
+
+}
