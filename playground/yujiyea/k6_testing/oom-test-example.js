a12defe585a7e6897640d28a33e9f15bedd0cac2
yujiyea <yje3002@naver.com>
2024-11-17 18:15:54 +0900

    week2(지예): 2주차 학습 테스트 추가

--- playground/yujiyea/k6_testing/oom-test-example.js
+++ playground/yujiyea/k6_testing/oom-test-example.js
@@ -1,5 +1,5 @@
 import http from "k6/http";
-import { sleep } from 'k6';
+import { check } from 'k6';
 
 export const options = {
     vus: 50,
@@ -8,10 +8,9 @@ export const options = {
 }
 
 export default async function() {
-    const url = 'http://localhost:8080/external/countries';
-    call(url)
-}
-
-async function call(url) {
-    http.get(url);
+    const baseUrl = 'http://host.docker.internal:8080/external/countries';
+    const res = http.get(baseUrl);
+    check(res, {
+        'Post status is not 200': (r) => res.status !== 200
+    })
 }
