443f6a4e5b85e35fb0a45554db60ab46c5d16389
JUNHWAN JANG <jhjang2403@tonymoly.com>
2024-11-19 17:34:46 +0900

    week3(준환): webflux 모니터링 추가 및 메트릭 이미지 추가

--- activity/week3/itschrisjang/k6/script/script.js
+++ activity/week3/itschrisjang/k6/script/script.js
@@ -1,17 +1,20 @@
 import http from "k6/http";
-import { sleep } from 'k6';
+import { sleep, check } from 'k6';
 
 export const options = {
-    vus: 50,
-    duration: '60s',
-    noConnectionReuse: true,
-}
+    scenarios: {
+        constant_rps: {
+            executor: 'constant-arrival-rate',
+            rate: 1000, // 초당 50 요청
+            timeUnit: '1s', // 초 기준
+            duration: '1m',
+            preAllocatedVUs: 10, // 사전에 할당할 VU 수
+            maxVUs: 100, // 최대 VU 수
+        },
+    },
+};
 
-export default async function() {
-    const url = 'http://localhost:8080/api/orders/1';
-    call(url)    
-}
-
-async function call(url) {
-    http.get(url);
+export default function () {
+    const res = http.get('http://localhost:8080/api/orders/7');
+    check(res, { 'status is 200': (r) => r.status === 200 });
 }
