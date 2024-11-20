0bf810d4491ba7a9ec90c22aefc48c5cae04cd0d
jeonghwaheo <jeonghwaheo@jobis.co>
2024-11-21 01:05:40 +0900

    refactor: orderId 인덱스 설정

--- playground/hoa0217/java-springboot-order-perf-optimizer/src/main/resources/schema.sql
+++ playground/hoa0217/java-springboot-order-perf-optimizer/src/main/resources/schema.sql
@@ -69,3 +69,15 @@ CREATE TABLE refunds
     refund_method_name VARCHAR(255),
     refund_status      VARCHAR(255)
 );
+
+-- Deliverys 테이블에 인덱스 추가
+CREATE INDEX idx_deliverys_order_id ON deliverys(order_id);
+
+-- Order_products 테이블에 인덱스 추가
+CREATE INDEX idx_order_products_order_id ON order_products(order_id);
+
+-- Payments 테이블에 인덱스 추가
+CREATE INDEX idx_payments_order_id ON payments(order_id);
+
+-- Refunds 테이블에 인덱스 추가
+CREATE INDEX idx_refunds_order_id ON refunds(order_id);
