cd4add20569908f2980d49a1510de8f0cab41e78
조상준 <129354526+sangjun121@users.noreply.github.com>
2024-11-30 21:17:05 +0900

    week4(상준): 주문API 설계 시 비동기 메소드와 쿼리 최적화 (#34)
    * feat: 주문 조회 동기 비즈니스 로직 구현 완료
    
    * feat: 동기 비동기 테스트 코드 작성

--- playground/sangjun121/java-springboot-order-perf-optimizer/src/main/java/com/order/perf/application/OrderService.java
+++ playground/sangjun121/java-springboot-order-perf-optimizer/src/main/java/com/order/perf/application/OrderService.java
@@ -3,15 +3,19 @@ package com.order.perf.application;
 import com.order.perf.domain.*;
 import com.order.perf.domain.repository.*;
 import com.order.perf.application.dto.OrderDetailsResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
+import static java.lang.Thread.sleep;
+
+@Slf4j
 @Transactional(readOnly = true)
 @Service
-@Async
 public class OrderService {
 
     private final OrderRepository orderRepository;
@@ -26,10 +30,27 @@ public class OrderService {
         this.refundRepository = refundRepository;
     }
 
-    public OrderDetailsResponse findOrderDetails(final Long orderId) {
+    public OrderDetailsResponse findOrderDetailsByAsync(final Long orderId) {
         Order order = orderRepository.findById(orderId)
                 .orElseThrow(() -> new RuntimeException("Order not found"));
 
+        CompletableFuture<Delivery> deliveryCompletableFuture = CompletableFuture.supplyAsync(() -> deliveryRepository.findById(order.getDeliveryId())
+                .orElseThrow(() -> new RuntimeException("Delivery not found")));
+
+        CompletableFuture<Refund> refundCompletableFuture = CompletableFuture.supplyAsync(() -> refundRepository.findById(order.getRefundId())
+                .orElseThrow(() -> new RuntimeException("Refund not found")));
+
+        CompletableFuture<List<OrderProduct>> orderProductsCompletableFuture
+                = CompletableFuture.supplyAsync(() -> order.getOrderProducts());
+
+        CompletableFuture.allOf(orderProductsCompletableFuture, deliveryCompletableFuture, refundCompletableFuture).join();
+
+        return new OrderDetailsResponse(orderProductsCompletableFuture.join(), deliveryCompletableFuture.join(), refundCompletableFuture.join());
+    }
+
+    public OrderDetailsResponse findOrderDetails(final Long orderId) {
+        Order order = orderRepository.findById(orderId)
+                .orElseThrow(() -> new RuntimeException("Order not found"));
 
         Delivery delivery = deliveryRepository.findById(order.getDeliveryId())
                 .orElseThrow(() -> new RuntimeException("Delivery not found"));
@@ -42,3 +63,4 @@ public class OrderService {
         return new OrderDetailsResponse(orderProducts, delivery, refund);
     }
 }
+
