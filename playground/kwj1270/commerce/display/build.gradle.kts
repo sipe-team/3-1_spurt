4d261845c00da6a42a4d17e106348c58da4370a7
woozi <kwj1270@naver.com>
2024-12-03 20:36:53 +0900

    feat(우재): 5주차 프로젝트 모니터링 시스템 구축

--- playground/kwj1270/commerce/display/build.gradle.kts
+++ playground/kwj1270/commerce/display/build.gradle.kts
@@ -21,6 +21,8 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    runtimeOnly("io.micrometer:micrometer-registry-prometheus")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
